2c513af0e765db5d2b5aceb914ade50055e12beb
mervesuilhn-coder <mervesuilhn@gmail.com>
2026-02-27 15:56:22 +0300

    Create using_CTE.sql

---
+++ using_CTE.sql
@@ -0,0 +1,18 @@
+
+WITH CTE_KategoriTutarlar� AS
+(
+SELECT 
+    u.UrunAdi,
+    k.KategoriAdi,
+    k.KategoriID,
+    ROUND(SUM(sd.BirimFiyati * sd.Miktar * (1 - sd.indirim)), 2) AS ToplamTutar,
+    ROW_NUMBER() OVER( PARTITION BY k.KategoriID ORDER BY SUM(sd.BirimFiyati * sd.Miktar * (1 - sd.indirim)) DESC) AS SiraNo
+FROM Urunler u
+JOIN Kategoriler k ON k.KategoriID = u.KategoriID
+JOIN SatisDetaylari sd ON u.UrunID = sd.UrunID 
+GROUP BY 
+    k.KategoriID,k.KategoriAdi,u.UrunAdi
+    ) 
+    SELECT TOP 3 C.ToplamTutar,C.KategoriAdi
+    FROM CTE_KategoriTutarlar� C
+    
