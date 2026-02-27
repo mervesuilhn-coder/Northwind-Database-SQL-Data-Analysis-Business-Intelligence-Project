0b0b1ce5d0108e5d6d18b736b4605cfd08d243c7
mervesuilhn-coder <mervesuilhn@gmail.com>
2026-02-27 15:42:58 +0300

    Delete sales_filtering_with_Between.sql

--- sales_filtering_with_Between.sql
+++
@@ -1,7 +0,0 @@
---Satis Sayisi 43 ile 100 arasında olan personeller
-SELECT p.PersonelID ,(p.Adi+' '+p.SoyAdi) PersonelAdSoyad,COUNT(*) SatisSayisi
-From Personeller p
-  JOIN Satislar s ON s.PersonelID=p.PersonelID 
-GROUP BY p.PersonelID,(p.Adi+' '+p.SoyAdi)
- HAVING COUNT(*) BETWEEN 43 AND 100
- ORDER BY PersonelAdSoyad ASC
